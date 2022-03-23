5c13753213f54705b63e4577abf959955bebde38
Oscar <>
2022-03-23 19:39:44 +1300

    add grid around grid

--- components/Categories/CategoryGrid.js
+++ components/Categories/CategoryGrid.js
@@ -3,11 +3,13 @@ import CategoryItem from './CategoryItem'
 
 const CategoryGrid = ({dummyShops}) => {
   return (
+    <div className={styles.cGrid}>
     <ul className={styles.categoryGrid}>
       {dummyShops.map(shop =>
         <CategoryItem key={shop.name} shop={shop} />
       )}
     </ul>
+    </div>
   )
 }
 
